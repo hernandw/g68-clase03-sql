f0b6d970b7a0efdb0059c902160890a07ea790db
Williams Hernandez <hernandw@gmail.com>
2024-04-10 18:58:38 -0400

    :tada: ejercios sql

---
+++ ejercicos.sql
@@ -0,0 +1,39 @@
+--Crear la base de datos TALENTO_DIGITAL
+--Crear la tabla G68, con los datos personales al menos rut, nombres, apellidos, edad, tlf, genero, sueldo (los tipos de datos lo determinan uds)
+
+--Insertar al menos 10 registros
+
+-- Listar toda la tabla
+
+
+-- Listar solo 3 campos de la tabla a su elección
+;
+
+-- Listar los primeros 5 registros creados
+
+
+-- Listar los alumnos con más de 25 años
+
+
+-- Listar rut, edad, tlf y sueldo para aquellos con sueldo de más de 300000 pesos
+
+
+-- Listar todos los campos para los de un género y que tengan menos de 30 años
+
+
+-- Listar todos los datos ordenados ascendente por edad
+
+
+-- Listar todos los datos ordenados descendente por sueldo
+
+
+-- Obtener el alumno con menor edad
+
+
+-- Obtener el alumno con mayor edad
+
+
+-- Listar todos los campos de los mayores de 20 años ordenado por nombre
+
+
+-- Listar todos los campos de los sueldos menores a 300000 ordenado por edad y solo los primeros 3.
